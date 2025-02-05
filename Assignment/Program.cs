8f21e8dca05114ce827d022f85d22552363e470f
Francesco Fiume <francesco.fiume2@gmail.com>
2025-02-05 17:04:47 +0100

    initial

---
+++ Assignment/Program.cs
@@ -0,0 +1,39 @@
+
+
+using Assignment.Data;
+using Assignment.Data.Collections;
+using Assignment.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+//Controller Registration
+builder.Services.AddControllers();
+
+//Swagger setup
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddEntityFrameworkNpgsql()
+    .AddDbContext<AppDbContext>(options =>
+    {
+        options.UseNpgsql(
+            builder.Configuration.GetConnectionString("DefaultConnection"),
+            o=> o.MapEnum<Books.Availability>("availability") 
+        );
+    });
+//Singleton Area
+builder.Services.AddSingleton<CustomerCollection>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
+
