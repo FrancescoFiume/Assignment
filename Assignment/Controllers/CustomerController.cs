7c14751484098ee7a8832231cc4d5476d61835e7
Francesco Fiume <francesco.fiume2@gmail.com>
2025-02-05 23:50:08 +0100

    Completed documentation for swagger and completed all the CRUD operations, now you will be able to get one or all the elements, create a new customer, update a customer, and delete a customer. Everything has logs and robust error catching as requested.

--- Assignment/Controllers/CustomerController.cs
+++ Assignment/Controllers/CustomerController.cs
@@ -1,7 +1,9 @@
+using System.Reflection;
 using Assignment.CustomExceptions;
 using Assignment.Data.Collections;
+using Assignment.Data.Models;
 using Assignment.DataCheck;
-using Assignment.DataCheck.Checks;
+using Assignment.DataCheck.Checks.CustomerChecks;
 using Assignment.DTO;
 using Microsoft.AspNetCore.Mvc;
 
@@ -19,13 +21,23 @@ public class CustomerController :ControllerBase
         _customerCollection = customerCollection;
         _logger = logger;
     }
-
+    
+    /// <summary>
+    /// Fetch all the Customers
+    /// </summary>
+    /// <returns>returns the whole collection as is</returns>
+    /// 
     [HttpGet]
     public IActionResult All()
     {
         return Ok(_customerCollection);
     }
 
+    /// <summary>
+    /// Fetch specific customer by Id, if the user doesn't exist returns NotFound
+    /// </summary>
+    /// <param name="id">Id must be an integer</param>
+    /// <returns>returns the user that has the requested id</returns>
     [HttpGet("{id}")]
     public  IActionResult GetOne(int id)
     {
@@ -36,34 +48,164 @@ public class CustomerController :ControllerBase
         }
         return Ok(user);
     }
-
+    
     /// <summary>
-    /// Ciao sto documentando
+    /// Add new Customer
     /// </summary>
-    /// <param name="newCustomer"></param>
-    /// <returns></returns>
-    [HttpPost("/new")]
+    /// <param name="newCustomer">
+    ///NewCustomer will be converted in a Struct, it needs to have:<br/>
+    /// First Name<br/>
+    /// Last Name<br/>
+    /// Mail<br/>
+    /// NB: The mail will be checked for both pattern and duplication in the codebase.
+    /// </param>
+    /// <returns>
+    /// Returns 200 and the newly created user Id if everything goes straight,<br/>
+    /// BadRequest if the mail is invalid or present already in the codebase
+    /// </returns>
+    [HttpPost("new")]
     public IActionResult Add(NewCustomer newCustomer)
     {
         try
         {
-            new Manager(new EmailCheck(_customerCollection), newCustomer).Check();
+            var newCustomerData = new Customers()
+            {
+                FirstName = newCustomer.FirstName,
+                LastName = newCustomer.LastName,
+                Email = newCustomer.Email,
+            };
+            HandleEmailValidation(newCustomerData);
+            var newAcc = _customerCollection.Add(newCustomerData);
+            return Ok(newAcc.Id);
+            //If this were an actual registration this should have returned
+            //1) Id
+            //2)Redirect page (The redirected page is a static page asking for mail to be verified.
         }
         catch (InvalidEmailFormatExceptions)
         {
             _logger.LogError("Invalid email format");
-            return BadRequest();
+            return BadRequest("Invalid email format");
         }
         catch (DuplicatedMailException)
         {
             _logger.LogError("Duplicated mail");
-            return BadRequest();
+            return BadRequest("Duplicated mail");
         }
-        
-        return Ok();
+        catch (Exception ex)
+        {
+            _logger.LogError(ex.Message);
+            return BadRequest(ex.Message);
+        }
     }
-    
-    
 
-    
+    /// <summary>
+    /// Change one or more properties on the underlying object
+    /// </summary>
+    /// <param name="newCustomer">
+    /// This item must contain the Id and
+    /// the string/strings that you want to modify.<br/>
+    /// If you only want to change one field just send the Id<br/>
+    /// and the string you want to change
+    /// </param>
+    /// <returns>
+    /// Not Found if the Id does not exist in the database,<br/>
+    /// Not Found if the email that you want to set is either incorrect or duplicated<br/>
+    /// No Content (code 204) if everything goes smoothly
+    /// </returns>
+    [HttpPut("update")]
+    public IActionResult Update(UpdateCustomer newCustomer)
+    {
+        if (_customerCollection.FirstOrDefault(c => c.Id == newCustomer.Id) == null)
+        {
+            _logger.LogError("Id not found");
+            return NotFound("Id not found");
+        }
+        Customers toUpdate = new Customers
+        {
+            Id = newCustomer.Id,
+            FirstName = "",
+            LastName = "",
+            Email = "",
+        };
+        Type userType = newCustomer.GetType();
+        PropertyInfo[] properties = userType.GetProperties();
+        try
+        {
+            foreach (var property in properties)
+            {
+                if (property.GetValue(newCustomer) != null)
+                {
+                    if (property.Name == "Email")
+                    {
+                        try
+                        {
+                            HandleEmailValidation(new Customers()
+                            {
+                                FirstName = "",
+                                LastName = "",
+                                Email = newCustomer.Email!,
+                            });
+                        }
+                        catch (InvalidEmailFormatExceptions)
+                        {
+                            _logger.LogError("Invalid email format");
+                            return BadRequest("Invalid email format");
+                        }
+                        catch (DuplicatedMailException)
+                        {
+                            _logger.LogError("Duplicated mail");
+                            return BadRequest("Duplicated mail");
+                        }
+                    }
+                    var toUpdateProperty = typeof(Customers).GetProperty(property.Name);
+                    toUpdateProperty!.SetValue(toUpdate, property.GetValue(newCustomer));
+
+                }
+            }
+            _customerCollection.Update(toUpdate);
+            return NoContent();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex.Message);
+            return BadRequest(ex.Message);
+        }
+    }
+
+    /// <summary>
+    /// Delete one element from the Database
+    /// </summary>
+    /// <param name="id">
+    ///Id is just the int of the element that you want to delete.
+    /// </param>
+    /// <returns>
+    ///Returns:<br/>
+    ///Not Found if the Id doesn't exist,<br/>
+    /// No Content (status 204) if the id exist and the deletion goes ok
+    /// </returns>
+    [HttpDelete("delete/{id}")]
+    public IActionResult Delete(int id)
+    {
+        try
+        {
+            if (_customerCollection.FirstOrDefault(c => c.Id == id) == null)
+            {
+                _logger.LogError("Id not found");
+                return NotFound("Id not found");
+            }
+            _customerCollection.Delete(id);
+            return NoContent();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex.Message);
+            return BadRequest(ex.Message);
+        }
+    }
+
+
+    private void HandleEmailValidation(Customers customer)
+    {
+            new Manager(new EmailCheck(_customerCollection), customer).Check();
+    }
 }
