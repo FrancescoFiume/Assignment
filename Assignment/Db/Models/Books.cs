7c7db4d19882ad1daf7122967ba7dd53aa5dee76
Francesco Fiume <francesco.fiume2@gmail.com>
2025-02-06 01:47:55 +0100

    removed required (not needed since those two are generated automatically).

--- Assignment/Db/Models/Books.cs
+++ Assignment/Db/Models/Books.cs
@@ -3,12 +3,12 @@ namespace Assignment.Data.Models;
 #pragma warning disable CS1591
 public class Books:IObjectDb
 {
-    public required int Id { get; set; } 
+    public int Id { get; set; } 
     public required string Title { get; set; }
     public required string Author { get; set; }
     
     public required string ISBN { get; set; }
-    public required Availability Status { get; set; }
+    public Availability Status { get; set; }
     public enum Availability : ushort
     {
         Unavailable=0,
