a4a4abb32390b3c0ae4fb1de182e200f369ba1fa
Francesco Fiume <francesco.fiume2@gmail.com>
2025-02-06 02:35:28 +0100

    DTO

---
+++ Assignment/DTO/NewBook.cs
@@ -0,0 +1,8 @@
+namespace Assignment.DTO;
+#pragma warning disable CS1591
+public struct NewBook
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string ISBN { get; set; }
+}
---
+++ Assignment/DTO/UpdateBook.cs
@@ -0,0 +1,9 @@
+namespace Assignment.DTO;
+#pragma warning disable CS1591
+public struct UpdateBook
+{
+    public int Id{get;set;}
+    public string? Title { get; set; }
+    public string? Author { get; set; }
+    public string? ISBN { get; set; }
+}
