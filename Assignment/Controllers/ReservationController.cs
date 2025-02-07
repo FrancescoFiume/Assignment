40aa4b250712a0d50019078d90ba153a79460d7e
Francesco Fiume <francesco.fiume2@gmail.com>
2025-02-07 03:41:49 +0100

    Completed endpoints for reservations (all CRUD and requested)

--- Assignment/Controllers/ReservationController.cs
+++ Assignment/Controllers/ReservationController.cs
@@ -1,5 +1,11 @@
+using Assignment.CustomExceptions;
 using Assignment.Data.Collections;
+using Assignment.Data.Models;
+using Assignment.DataCheck;
+using Assignment.DataCheck.Checks.ReservationCheck;
+using Assignment.DTO;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Assignment.Controllers;
 /// <summary>
@@ -10,20 +16,27 @@ namespace Assignment.Controllers;
 public class ReservationController:ControllerBase
 {
     private readonly ReservationCollection _reservationCollection;
+    private readonly CustomerCollection _customerCollection;
+    private readonly BookCollection _bookCollection;
     private readonly ILogger<CustomerController> _logger;
     /// <summary>
     /// Constructor
     /// </summary>
     /// <param name="reservationCollection">Dependency Injected books Collection</param>
     /// <param name="logger">Dependency Injected logger</param>
-    public ReservationController(ReservationCollection reservationCollection, ILogger<CustomerController> logger)
+    public ReservationController(ReservationCollection reservationCollection, 
+        ILogger<CustomerController> logger,
+        CustomerCollection customerCollection,
+        BookCollection bookCollection)
     {
         _logger = logger;
         _reservationCollection = reservationCollection;
+        _customerCollection = customerCollection;
+        _bookCollection = bookCollection;
         
     }
     /// <summary>
-    /// Fetch all the Books
+    /// Fetch all the reservations
     /// </summary>
     /// <response code="200">Returns list of reservation, each one has book and user data</response>
     /// <response code="400">Something went wrong</response>
@@ -64,6 +77,98 @@ public class ReservationController:ControllerBase
 
             _logger.LogError("Customer has no reservations");
             return NotFound("Customer has no reservations");
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e.Message);
+            return BadRequest(e.Message);
+        }
+    }
+    /// <summary>
+    /// Create a new Reservation
+    /// </summary>
+    /// <param name="newReservation">this object needs to have only two valid fields, Book Id and Customer Id</param>
+    /// <response code="201">Reservation created, you will get back the whole object with also the Book and Customer details</response>
+    /// <response code="400">One of the following happened: BookId Incorrect, CustomerId Incorrect, or something else happened, read body</response>
+    [HttpPost]
+    public IActionResult Add(NewReservation newReservation)
+    {
+        var reservation = new Reservations()
+        {
+            BookId = newReservation.BookId,
+            CustomerId = newReservation.CustomerId,
+        };
+        try
+        {
+            new Manager(new BookReservedChecks(_reservationCollection, newReservation.BookId), reservation).Check();
+           reservation =  _reservationCollection.Add(reservation);
+            return Created("redirect Url", reservation);
+            
+        }
+        catch (BookReservedException ex)
+        {
+            _logger.LogError(ex.Message);
+            return BadRequest("Book reservation already exists");
+        }
+        //I'm delegating to the DB to check if the id of customer and book are correct, if they are not, then it will throw either one of those
+        catch (Exception e) when (e is DbUpdateException || e is DbUpdateConcurrencyException)
+        {
+            _logger.LogError(e.Message);
+            return BadRequest("BookId or Customer Id is not valid");
+        }
+    }
+
+    /// <summary>
+    /// Extend expiration Date
+    /// </summary>
+    /// <param name="reservationId">Id of the reservation that you want to extend</param>
+    /// <param name="newDate">new date(must be YYYY-MM-DD)</param>
+    /// <response code="204">Reservation created updated successfully</response>
+    /// <response code="400">Something went wrong, read body</response>
+    /// <response code="404">Reservation Id doesn't exist</response>
+
+
+    [HttpPut("{reservationId}")]
+    public IActionResult ExtendReservation([FromRoute] int reservationId, DateTime newDate)
+    {
+        try
+        {
+            _reservationCollection.GetById(reservationId);
+            _reservationCollection.SetCustomExpiration(reservationId, newDate);
+            return NoContent();
+        }
+        catch (Exception e) when (e is ArgumentNullException || e is InvalidOperationException)
+        {
+            _logger.LogError(e.Message);
+            return NotFound("Reservation Id doesn't exist");
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e.Message);
+            return BadRequest(e.Message);
+        }
+    }
+
+    /// <summary>
+    /// Delete Reservation
+    /// </summary>
+    /// <param name="reservationId">Id of the reservation that you want to delete</param>
+    /// <response code="204">Item deleted successfully</response>
+    /// <response code="400">Something went wrong, read body</response>
+    /// <response code="404">Reservation Id doesn't exist</response>
+    [HttpDelete("{reservationId}")]
+    public IActionResult RemoveReservation([FromRoute] int reservationId)
+    {
+        try
+        {
+            _reservationCollection.GetById(reservationId);
+            _reservationCollection.Delete(reservationId);
+            return NoContent();
+        }
+        catch (Exception e) when (e is ArgumentNullException || e is InvalidOperationException)
+        {
+            _logger.LogError(e.Message);
+            return NotFound("Reservation Id doesn't exist");
         }
         catch (Exception e)
         {
---
+++ Assignment/DTO/NewReservation.cs
@@ -0,0 +1,7 @@
+namespace Assignment.DTO;
+
+public struct NewReservation
+{
+    public int CustomerId { get; set; }
+    public int BookId { get; set; }
+}
